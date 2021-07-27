4fe45d8ea7d0a920960ce4c6ed10f45bb04fd5d7
scott <scottwoolfrey@gmail.com>
2021-07-27 10:06:09 -0230

    setup

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 from random import randint
 
 
-class Cards:
-    def __init__(self):
-        pass
+class Card:
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+
+    def show(self):
+        print("{} of {}".format(self.value, self.suit))
 
 
 class Deck:
     def __init__(self):
-        pass
+        self.cards = []
+        self.build()
+
+    def build(self):
+
 
 
 class Player:
     def __init__(self):
         pass
 
+
+card = Card("Card", 6)
+card.show()
