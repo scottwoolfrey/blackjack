58f0deecccb06a1d3e2c9bef6ebf6cb88fb3a1e7
scott <scottwoolfrey@gmail.com>
2021-07-27 10:15:38 -0230

    setup

--- main.py
+++ main.py
@@ -16,6 +16,18 @@ class Deck:
         self.build()
 
     def build(self):
+        for s in ["Spades", "Clubs", "Diamonds", "Hearts"]:
+            for n in range(1, 14):
+                self.cards.append(Card(s, n))
+
+    def show(self):
+        for cd in self.cards:
+            cd.show()
+
+
+    def shuffle_deck(self):
+        for sd in range(len(self.cards) -1, 0, -1):
+
 
 
 
@@ -24,5 +36,5 @@ class Player:
         pass
 
 
-card = Card("Card", 6)
-card.show()
+deck = Deck()
+deck.show()
