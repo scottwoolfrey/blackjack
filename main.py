23451cdd4c97f9353b3f9bd2259818a63d187a5f
scott <scottwoolfrey@gmail.com>
2021-07-27 12:06:40 -0230

    Changes by Driven Powersports

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 import numpy as np
 
-
+print(np.arange(9))
 class Suites:
     def __init__(self):
         self.type = ["A", "2", "3","4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
