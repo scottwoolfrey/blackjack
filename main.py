bc4008100c61437f4eaf16cf0022974e3d13b98d
scott <scottwoolfrey@gmail.com>
2021-07-27 12:04:05 -0230

    stuff

--- main.py
+++ main.py
@@ -1,40 +1,34 @@
-from random import randint
+import random
+import numpy as np
 
 
-class Card:
-    def __init__(self, suit, value):
-        self.suit = suit
-        self.value = value
-
-    def show(self):
-        print("{} of {}".format(self.value, self.suit))
-
-
-class Deck:
+class Suites:
     def __init__(self):
-        self.cards = []
-        self.build()
+        self.type = ["A", "2", "3","4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+        
+        self.cards = [[suite + num for num in self.type] for suite in ["H","S","D","C"]]
+        self.gameCards = self.cards.copy()
+        self.value = [0,2,3,4,5,6,7,8,9,10,10,10,10]
+        
+    def draw(self):
+        for i in range(52):
+            suite = random.choice(self.cards)
+            num = random.choice(suite)
+            print(num)
+            suite.remove(num)
+            print("elements in list:", len(suite))
+            
+            # if the suite list is empty
+            if not suite:
+                self.cards.remove(suite)
 
-    def build(self):
-        for s in ["Spades", "Clubs", "Diamonds", "Hearts"]:
-            for n in range(1, 14):
-                self.cards.append(Card(s, n))
-
-    def show(self):
-        for cd in self.cards:
-            cd.show()
-
-
-    def shuffle_deck(self):
-        for sd in range(len(self.cards) -1, 0, -1):
-
-
-
+s = Suites()
+s.draw()
 
 class Player:
     def __init__(self):
         pass
-
-
-deck = Deck()
-deck.show()
+    
+    def score(self):
+        pass
+#tot_score = 
