f2966c745c1237f127a69b52c99c91d708fbef15
scott <scottwoolfrey@gmail.com>
2021-07-27 09:53:15 -0230

    setup

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-# test
+from random import randint
+
+
+class Cards:
+    def __init__(self):
+        pass
+
+
+class Deck:
+    def __init__(self):
+        pass
+
+
+class Player:
+    def __init__(self):
+        pass
+
